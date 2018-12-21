e750272283cf9be5dec4e61452e08b2bbb280fb4
Joshua Tzucker <joshua.tz@gmail.com>
2018-12-20 17:08:56 -0800

    Setting up structure

--- source.js
+++ source.js
@@ -2,3 +2,32 @@
  * @file Unminified / Unescaped raw JS behind bookmarklet for adding non-Amazon product page to Amazon Wishlist
  * @author Joshua Tzucker
  */
+
+function addToAmazonWishlist(debug){
+    var selectedProductDetails = {
+        productName : '',
+        productPrice : '$0.00',
+        requestedQty : 1,
+        asin : '',
+        productUrl : '',
+        comment : '',
+        imageUrl : '',
+        registryID : '',
+        type : 'wishlist'
+    }
+    function run(){
+
+    }
+    function showProductInputPanel(){
+
+    }
+    function productDetector($opt_DomElement){
+
+    }
+
+    return {
+        run : run
+    }
+}
+
+new addToAmazonWishlist(true).run();
